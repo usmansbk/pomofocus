65b4f4072a0bb9c8b21ce63174c50a7f3391a06f
Usman Suleiman <usmansbk@gmail.com>
2021-09-29 15:37:02 +0100

    Style container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Header from "./components/Header";
 import classes from "./App.module.css";
+import Progress from "./components/Progress";
+import Timer from "./components/Timer";
 
 function App() {
   return (
     <div className={classes.container}>
       <Header />
+      <div className={classes.content}>
+        <div className={classes.body}>
+          <Progress />
+          <Timer />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Progress.js
@@ -0,0 +1,5 @@
+import classes from "./Progress.module.css";
+
+export default function Progress() {
+  return <div className={classes.container}></div>;
+}

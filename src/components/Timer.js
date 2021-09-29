522e5aa48296cb0b312a57cf69548e3d9ff23fc6
Usman Suleiman <usmansbk@gmail.com>
2021-09-29 16:01:18 +0100

    Style radio button

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,9 +1,34 @@
 import classes from "./Timer.module.css";
 
+const RadioButton = ({ children }) => {
+  return <button className={classes.radioButton}>{children}</button>;
+};
+
+const buttons = [
+  {
+    id: "pomodoro",
+    label: "Pomodoro",
+  },
+  {
+    id: "short-break",
+    label: "Short Break",
+  },
+  {
+    id: "long-break",
+    label: "Long Break",
+  },
+];
+
 export default function Timer() {
   return (
     <div className={classes.container}>
-      <div className={classes.content}></div>
+      <div className={classes.content}>
+        <ul>
+          {buttons.map(({ id, label }) => (
+            <RadioButton id={id}>{label}</RadioButton>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }

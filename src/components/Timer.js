de9cd13ef966433261e10dc54c56b41281789895
Usman Suleiman <usmansbk@gmail.com>
2021-09-29 20:29:44 +0100

    Add ui logic

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import clsx from "clsx";
 import Icon from "./Icon";
 import classes from "./Timer.module.css";
 
-const SecondaryButton = ({ children, active }) => {
+const SecondaryButton = ({ children, active, onClick }) => {
   return (
     <button
+      onClick={onClick}
       className={clsx(
         classes.secondaryButton,
         active && classes.secondaryActive
@@ -15,16 +17,17 @@ const SecondaryButton = ({ children, active }) => {
   );
 };
 
-const PrimaryButton = ({ active }) => (
+const PrimaryButton = ({ active, onClick }) => (
   <button
+    onClick={onClick}
     className={clsx(classes.primaryButton, active && classes.primaryActive)}
   >
     {active ? "Stop" : "Start"}
   </button>
 );
 
-const SkipButton = () => (
-  <button className={classes.skipButton}>
+const SkipButton = ({ onClick }) => (
+  <button onClick={onClick} className={classes.skipButton}>
     <Icon name="skip_next" size={48} />
   </button>
 );
@@ -45,28 +48,35 @@ const buttons = [
 ];
 
 export default function Timer() {
-  const mode = "pomodoro";
-  const time = "25:00";
-  const round = 1;
-  const isRunning = false;
+  const [mode, setMode] = useState("pomodoro");
+  const [time] = useState("25:00");
+  const [round, setRound] = useState(1);
+  const [isRunning, setRunning] = useState(false);
 
   return (
     <div className={classes.container}>
       <div className={classes.content}>
         <ul>
           {buttons.map(({ id, label }) => (
-            <SecondaryButton active={id === mode} id={id}>
+            <SecondaryButton
+              active={id === mode}
+              id={id}
+              onClick={() => setMode(id)}
+            >
               {label}
             </SecondaryButton>
           ))}
         </ul>
         <div className={classes.time}>{time}</div>
         <div className={classes.actionButtons}>
-          <PrimaryButton active={isRunning} />
+          <PrimaryButton
+            active={isRunning}
+            onClick={() => setRunning(!isRunning)}
+          />
           <div
             className={clsx(classes.skipAction, isRunning && classes.showSkip)}
           >
-            <SkipButton />
+            <SkipButton onClick={() => setRound(round + 1)} />
           </div>
         </div>
       </div>

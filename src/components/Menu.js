ce0a7dd8553505a94e387ad1996c2c7671702c70
Usman Suleiman <usmansbk@gmail.com>
2021-09-29 23:44:33 +0100

    Menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,13 +6,16 @@ export default function Menu({ children, menuButton }) {
   const [visible, setVisible] = useState(false);
   const onDismiss = useCallback(() => setVisible(false), []);
   const onOpen = useCallback((e) => {
-    setVisible(true);
+    setVisible((prev) => !prev);
     e.stopPropagation();
   }, []);
 
   return (
-    <div onClick={onDismiss}>
-      <div className={clsx(classes.overlay, visible && classes.visible)} />
+    <div>
+      <div
+        onClick={onDismiss}
+        className={clsx(classes.overlay, visible && classes.visible)}
+      />
       <div className={classes.menuContent}>
         {menuButton(onOpen)}
         <div className={clsx(classes.container, visible && classes.visible)}>

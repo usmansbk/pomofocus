18286c4af1c27e901bcbdbaf97dc2e7d416102e1
Usman Suleiman <usmansbk@gmail.com>
2021-09-30 17:11:40 +0100

    Fix close on request

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,18 +1,31 @@
+import { useEffect, useRef } from "react";
 import { useHistory } from "react-router";
 import Icon from "./Icon";
 import classes from "./Modal.module.css";
 
 export default function Modal({ children }) {
+  const modalRef = useRef(null);
   const history = useHistory();
   const back = (e) => {
     e.stopPropagation();
     history.goBack();
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        history.goBack();
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [history]);
+
   return (
     <div className={classes.container}>
-      <div className={classes.overlay} onClick={back} />
-      <div className={classes.modal}>
+      <div className={classes.overlay} />
+      <div className={classes.modal} ref={modalRef}>
         {children}
         <button className={classes.closeButton} onClick={back}>
           <Icon name="close" />

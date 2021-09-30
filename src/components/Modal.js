8cf93511293ae96d9102fd3372d2853aa6761d94
Usman Suleiman <usmansbk@gmail.com>
2021-09-30 04:00:55 +0100

    Add close logic

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,23 @@
+import { useHistory } from "react-router";
+import Icon from "./Icon";
 import classes from "./Modal.module.css";
 
 export default function Modal({ children }) {
+  const history = useHistory();
+  const back = (e) => {
+    e.stopPropagation();
+    history.goBack();
+  };
+
   return (
     <div className={classes.container}>
-      <div className={classes.overlay} />
-      <div className={classes.content}>{children}</div>
+      <div className={classes.overlay} onClick={back} />
+      <div className={classes.content}>
+        <button className={classes.closeButton} onClick={back}>
+          <Icon name="close" />
+        </button>
+        {children}
+      </div>
     </div>
   );
 }

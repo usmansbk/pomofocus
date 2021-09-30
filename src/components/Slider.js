5ec0f50a36dca0093f1730564f37bb54e1da9a94
Usman Suleiman <usmansbk@gmail.com>
2021-09-30 18:15:16 +0100

    Add form control

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import classes from "./Slider.module.css";
 
 export default function Slider() {
-  const value = 0;
+  const [value, setValue] = useState(0);
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div className={classes.container}>
       <span className={classes.label}>{value}</span>
-      <input type="range" min={0} max={100} className={classes.input} />
+      <input
+        value={value}
+        onChange={handleChange}
+        type="range"
+        min={0}
+        max={100}
+        className={classes.input}
+      />
     </div>
   );
 }

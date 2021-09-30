3b38820fad0b6b5966c1cf68b152736c9e26ce1f
Usman Suleiman <usmansbk@gmail.com>
2021-09-30 10:12:32 +0100

    Style modal

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,13 +1,64 @@
+import clsx from "clsx";
+import { useHistory } from "react-router";
 import Modal from "./Modal";
 import classes from "./Settings.module.css";
 
+const Item = ({ children, col }) => (
+  <div className={clsx(classes.item, col && classes.column)}>{children}</div>
+);
+
+const Label = ({ children }) => (
+  <label className={classes.label}>{children}</label>
+);
+
+const Button = ({ children, onClick }) => (
+  <button className={classes.button} onClick={onClick}>
+    {children}
+  </button>
+);
+
 export default function Settings() {
+  const history = useHistory();
+  const back = (e) => {
+    e.stopPropagation();
+    history.goBack();
+  };
+
   return (
     <Modal>
       <div className={classes.container}>
         <div className={classes.content}>
           <h2 className={classes.title}>Timer Settings</h2>
+          <div>
+            <Item>
+              <Label>Time (minutes)</Label>
+            </Item>
+            <Item>
+              <Label>Auto start Breaks?</Label>
+            </Item>
+            <Item>
+              <Label>Auto start Pomodoros?</Label>
+            </Item>
+            <Item>
+              <Label>Long Break interval</Label>
+            </Item>
+            <Item>
+              <Label>Alarm Sound</Label>
+            </Item>
+            <Item>
+              <Label>Ticking Sound</Label>
+            </Item>
+            <Item>
+              <Label>Dark Mode when running</Label>
+            </Item>
+            <Item>
+              <Label>Notification</Label>
+            </Item>
+          </div>
         </div>
+        <footer className={classes.footer}>
+          <Button onClick={back}>OK</Button>
+        </footer>
       </div>
     </Modal>
   );

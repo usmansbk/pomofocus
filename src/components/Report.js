cda22210aa0778820487a8087f008bf726275203
Usman Suleiman <usmansbk@gmail.com>
2021-10-01 09:06:40 +0100

    Style report header

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,13 +1,38 @@
 import Modal from "./Modal";
 import classes from "./Report.module.css";
 
+const tabs = [
+  {
+    id: "summary",
+    label: "Summary",
+  },
+  {
+    id: "detail",
+    label: "Detail",
+  },
+  {
+    id: "ranking",
+    label: "Ranking",
+  },
+];
+
+const TabItem = ({ label }) => (
+  <button className={classes.tabItem}>{label}</button>
+);
+
+const TabBar = () => (
+  <header className={classes.header}>
+    {tabs.map(({ id, label }) => (
+      <TabItem key={id} label={label} />
+    ))}
+  </header>
+);
+
 export default function Report() {
   return (
     <Modal>
       <div className={classes.container}>
-        <header className={classes.header}>
-          <h2>Page Under Construction</h2>
-        </header>
+        <TabBar />
       </div>
     </Modal>
   );

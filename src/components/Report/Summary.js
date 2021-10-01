7e23efc3883bda0921011b4570571ebbc37b8d2e
Usman Suleiman <usmansbk@gmail.com>
2021-10-01 12:08:58 +0100

    Style cards

--- src/components/Report/Summary.js
+++ src/components/Report/Summary.js
@@ -1,10 +1,37 @@
 import Title from "./Title";
+import Icon from "../Icon";
+import classes from "./Report.module.css";
+
+const Card = ({ icon, label, value = "--" }) => (
+  <div className={classes.card}>
+    <div className={classes.cardIcon}>
+      <Icon name={icon} size={32} />
+    </div>
+    <div className={classes.cardTitle}>{value}</div>
+    <div className={classes.cardLabel}>{label}</div>
+  </div>
+);
+
+const Cards = () => (
+  <div>
+    <div className={classes.noData}>
+      <p className={classes.note}>
+        * This report will be available when you are logged in
+      </p>
+    </div>
+    <div className={classes.cards}>
+      <Card icon="schedule" label="hours focused" />
+      <Card icon="date_range" label="days accessed" />
+      <Card icon="whatshot" label="day streak" />
+    </div>
+  </div>
+);
 
 export default function Summary() {
   return (
     <div>
       <Title>Activity Summary</Title>
-      <h1>Page Under Construction</h1>
+      <Cards />
     </div>
   );
 }

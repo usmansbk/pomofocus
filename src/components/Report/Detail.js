b81527138ca5fd0eb95d19be055ee00b02c6e3fb
Usman Suleiman <usmansbk@gmail.com>
2021-10-09 09:26:26 +0100

    Style modal table

--- src/components/Report/Detail.js
+++ src/components/Report/Detail.js
@@ -1,11 +1,24 @@
+import clsx from "clsx";
 import { NoData, Title } from "./common";
+import classes from "./Detail.module.css";
+
+function Table() {
+  return (
+    <table className={classes.table}>
+      <tr className={clsx(classes.row, classes.headerRow)}>
+        <th className={classes.header}>Date</th>
+        <th className={classes.header}>Minutes</th>
+      </tr>
+    </table>
+  );
+}
 
 export default function Detail() {
   return (
-    <div>
+    <div className={classes.container}>
       <Title>Focus Time Detail</Title>
       <NoData />
-      <h1>Detail</h1>
+      <Table />
     </div>
   );
 }
